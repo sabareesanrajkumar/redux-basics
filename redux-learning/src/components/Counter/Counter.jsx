1601c747284a94de251528988d7c6403badd3c8c
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2025-04-22 14:26:44 +0530

    react redux actions

---
+++ redux-learning/src/components/Counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Counter = () => {
+  const toggleCounterHandler = () => {};
+  return (
+    <div>
+      <h1>Redux cunter</h1>
+      <button onClick={toggleCounterHandler}>Toggle counter</button>
+    </div>
+  );
+};
+
+export default Counter;
